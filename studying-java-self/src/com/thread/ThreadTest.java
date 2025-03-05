da6d5f43af18788736cd43e25a33a179c6b6558f
Ealsen_Huangfu <111371035+Ealsen@users.noreply.github.com>
2025-03-05 19:06:11 +0800

    refactor(thread): 更新 ThreadTest 类并添加学习资源链接
    - 在 ThreadTest.java 中添加了反射和注解的学习资源链接
    -移除了 Book 类的添加，简化了本次更新的内容
    - 更新了工作空间中的任务描述，专注于线程测试功能的添加

--- studying-java-self/src/com/thread/ThreadTest.java
+++ studying-java-self/src/com/thread/ThreadTest.java
@@ -19,7 +19,22 @@ public class ThreadTest {
         System.out.println("Main!");
     }
 }
+
+/*
+后面的章节懒得敲代码了，这里附上在线笔记链接
+ */
+
 /*
 线程
 https://www.itbaima.cn/document/lfqtvxr7azumcwja
+ */
+
+/*
+反射
+https://www.itbaima.cn/document/lfqtvxr7azumcwja?segment=2#doc8-%E5%8F%8D%E5%B0%84
+ */
+
+/*
+注解
+https://www.itbaima.cn/document/lfqtvxr7azumcwja?segment=3#doc8-%E9%A2%84%E8%AE%BE%E6%B3%A8%E8%A7%A3
  */
